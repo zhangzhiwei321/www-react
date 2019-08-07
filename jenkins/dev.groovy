e6c82602cf16f771f8d6828f7e7ca978aceb1a45
zhangzhiwei <zhangzhiwei@aibaoxian.com>
2019-08-07 16:28:35 +0800

    jenkins

---
+++ jenkins/dev.groovy
@@ -0,0 +1,30 @@
+node {
+	stage('check') {
+		echo("check");
+		echo(name);
+		sh 'ls'
+		sh 'git pull origin dev'
+		sh 'git pull origin dev'
+		sh 'git status'
+		sh 'git branch'
+		sh 'git checkout dev'
+	}
+	stage('install') {
+		echo("install")
+		echo(install);
+		if (install) {
+			sh "rm -rf node_modules*"
+			sh "cnpm i"
+		}
+	}
+	stage('build') {
+		echo("build")
+		sh "npm run build"
+	}
+
+	stage("release") {
+		echo("release")
+		sh "rm -rf /usr/local/nginx/web/vue/*"
+		sh "mv ./dist/* /www"
+	}
+}
