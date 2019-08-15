50ff5444fc255fcdbc8622be14c97acd22bbc97c
zhangzhiwei321 <33408997+zhangzhiwei321@users.noreply.github.com>
2019-08-15 17:16:12 +0800

    Update App.js
    提交图片

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import logo from 'https://6d69-missubear-developmen-1256962785.tcb.qcloud.la/images/af458f4e082a2f28611ab887fbe0da8e.jpg?sign=2cba34ba633b0790218d10e30318936b&t=1565851935';//'./logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
+let log='https://6d69-missubear-developmen-1256962785.tcb.qcloud.la/images/af458f4e082a2f28611ab887fbe0da8e.jpg?sign=2cba34ba633b0790218d10e30318936b&t=1565851935';
 function App() {
   return (
     <div className="App">
