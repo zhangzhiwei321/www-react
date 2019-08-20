7b5306dee8678a93f0ed9fad579b93d06365e8f2
zhangzhiwei <zhangzhiwei@aibaoxian.com>
2019-08-20 17:18:46 +0800

    favicon.ico

--- src/App.js
+++ src/App.js
@@ -2,30 +2,35 @@ import React from 'react';
 
 //import logo from './logo.svg';
 
-import logo from 'https://6d69-missubear-developmen-1256962785.tcb.qcloud.la/images/af458f4e082a2f28611ab887fbe0da8e.jpg?sign=2cba34ba633b0790218d10e30318936b&t=1565851935'//'./logo.svg';
-
 import './App.css';
-let logo='https://6d69-missubear-developmen-1256962785.tcb.qcloud.la/images/af458f4e082a2f28611ab887fbe0da8e.jpg?sign=2cba34ba633b0790218d10e30318936b&t=1565851935';
+let logo =
+	'https://6d69-missubear-developmen-1256962785.tcb.qcloud.la/images/af458f4e082a2f28611ab887fbe0da8e.jpg?sign=2cba34ba633b0790218d10e30318936b&t=1565851935';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-    <h2>ლ(′◉❥◉｀ლ)</h2>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return ( <
+		div className = "App" >
+		<
+		header className = "App-header" >
+		<
+		img src = {
+			logo
+		}
+		className = "App-logo"
+		alt = "logo" / >
+		<
+		h2 > ლ(′◉❥◉｀ლ) < /h2> <
+		p >
+		Edit < code > src / App.js < /code> and save to reload. < /
+		p > <
+		a className = "App-link"
+		href = "https://reactjs.org"
+		target = "_blank"
+		rel = "noopener noreferrer" >
+		Learn React <
+		/a> < /
+		header > <
+		/div>
+	);
 }
 
 export default App;
